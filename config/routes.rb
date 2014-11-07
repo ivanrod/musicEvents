1078053ff96a9056ae2201d10e692796bf1a65ed
Ivan Rodriguez <frivanrodriguez@gmail.com>
2014-11-07 11:22:25 +0100

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 MusicEvents::Application.routes.draw do
 
   root 'welcome#index'
+
+  resources :locations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
