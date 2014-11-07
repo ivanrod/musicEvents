d20db34f4f85e0b41afa1a709882777768372980
Ivan Rodriguez <frivanrodriguez@gmail.com>
2014-11-07 09:51:18 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicEvents::Application.config.secret_key_base = '928f9632cfd82cc8963479700e2c0905e8307079c847be235358fbed21a34a72a7bdd32a7cd74edb2a566aa84fcc78258fbdedc8cb3cf431ff893b25ddf779fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MusicEvents::Application.config.session_store :cookie_store, key: '_musicEvents_session'
